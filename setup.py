69e0d59f7f2b2d1b2236344bb46e12c831b3ed65
Matthew Thomson <m7homson@gmail.com>
2018-03-05 09:11:28 -0400

    adding setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'easy_gene',
+  packages = ['easy_gene'],
+  version = '0.1',
+  description = 'easy to use genetic algorythm',
+  author = 'Matthew Thomson ',
+  author_email = 'm7homson@gmail.com',
+  url = 'https://github.com/thomsn/easy_gene',
+  download_url = 'https://github.com/thomsn/easy_gene/archive/1.0.zip',
+  keywords = ['genetic', 'ml', 'machine', 'learning'],
+  classifiers = [],
+)
